ba50577bee03f891812dc658635b261db56ba0b1
EricCortez <65201469+EricCortez@users.noreply.github.com>
2021-04-13 00:58:50 -0400

    Add files via upload
    Actividad 10

---
+++ act10/act10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  act10
+//
+//  Created by user182851 on 4/12/21.
+//  Copyright © 2021 user182851. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
